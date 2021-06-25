8742bde9936d5bf4da643d68f74e7d8ef7c997ec
Nurettin Yıldız <53060153+Nuri-Yildiz@users.noreply.github.com>
2021-06-25 14:05:35 +0300

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,60 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Bulunulan ortamın durulup sakinleştiğini söylemek için hangi ifade kullanılır?",
+    answer: "Ortalık sütliman",
+    options: [
+      "Etraf kahveperon",
+      "Ortam çayotogar",
+      "Çevre ayrandurak",
+      "Ortalık sütliman"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Kimlere fındık kurdu denir?",
+    answer: "Ufak tombulca ve sevimli kişilere",
+    options: [
+      "İri yarı ve güçlü kişilere",
+      "Ufak tombulca ve sevimli kişilere",
+      "Uzun boylu ve zayıf kişilere",
+      "Geniş omuzlu ve yapılı kişilere",
+      "Fındık sevenlere"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Hangi Arap ülkesi Arap Yarımadası sınırları içinde yer almaz?",
+    answer: "Libya",
+    options: [
+      "Umman",
+      "Yemen",
+      "Katar",
+      "Libya"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Hangisi, Mona Lisa tablosunu yatak odasına asmıştır?",
+    answer: "Napolyon Bonapart",
+    options: [
+      "Napolyon Bonapart",
+      "Benito Mussolini",
+      "Adolf Hitler",
+      "Bismarck"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Futbolda, attığı pasla takım arkadaşını net bir gol pozisyonuna sokan oyuncular için spikerler genelde hangisini söyler?",
+    answer: "Al da at",
+    options: [
+      "Yeme de yanında yat",
+      "Al da at",
+      "Gör de söyleme",
+      "Çal da oynama"
+    ]
+  },
+
+];
